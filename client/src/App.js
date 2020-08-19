d55b2f75c9f7cbd54c00d2de040d53be3a38f913
aagrosse <aagrock81@yahoo.com>
2020-08-19 04:01:55 -0400

    added components

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import LandingPage from "./pages/landingPage"
+// import Admin from "./pages/Dashboard/Dashboard"
+
+function App() {
+    return (
+      <Router>
+        {/* <StoreProvider> */}
+          <Route exact path='/' component={LandingPage} />
+          {/* <Route exact path='/dashboard' component={Admin} /> */}
+        {/* </StoreProvider> */}
+      </Router>
+  );
+}
+
+export default App;
---
+++ client/src/pages/landingPage.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Navbar from "../components/Navbar"
+import Sidebar from "../components/Sidebar"
+import Footer from "../components/Footer"
+
+function LandingPage() {
+    return (
+        <div>
+            <Navbar />
+            <Sidebar />
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima aspernatur quo recusandae aliquam rem, expedita saepe nesciunt necessitatibus modi consectetur?
+            <Footer/>
+        </div>
+    )
+}
+
+export default LandingPage;
